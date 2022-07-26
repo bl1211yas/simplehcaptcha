f9ea60944ca9e6c7df81dddb572825f971eea0b0
bl1211yas <108929653+bl1211yas@users.noreply.github.com>
2022-07-26 10:28:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<php
+<head>
+<script src='https://www.hCaptcha.com/1/api.js' async defer></script>
+
+</head>
+<body>
+<h1>
+simple hcaptcha web simplementation
+</h1>
+<div class="h-captcha" data-sitekey="717ddd1b-bc1a-49ae-bf1c-f831d647f25c" ></div>
+</body>
